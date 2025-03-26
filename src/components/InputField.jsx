cd8429a6be753fb210e7cb4f821c439611f01ad3
Zach <zrfox97@gmail.com>
2025-03-26 13:17:09 -0700

    Add alert for binary only and calculate with  enter onKeyDown

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,12 +1,12 @@
 
-function InputField({value, onClick, onChange}){
+function InputField({value, onClick, onChange, onKeyDown}){
 
     //const [userInput, setUserInput] = useState('');
 
     return(
         <>
                     <label htmlFor="binary-input">Input Binary:
-                        <input type="text" id='binary-input' value={value} onChange={onChange} />
+                        <input type="text" className="binary-input" id='binary-input' value={value} onChange={onChange} onKeyDown={onKeyDown} />
                     </label>
                 <button onClick={onClick}>Calculate</button>
         </>
--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -7,7 +7,7 @@ import '../App.css'
 function Interface(){ //are these right? or should they be the other names?
     const [data, setData] = useState(null);
     const [input, setInput] = useState("");
-
+    const [showError, setShowError] = useState(false);
 
     const handleButtonClick = () => {
         const result = binaryToDecimal(input);
@@ -15,14 +15,35 @@ function Interface(){ //are these right? or should they be the other names?
     }
 
     const handleInputChange = (event) => {
-        setInput(event.target.value);
+        const value = event.target.value;
+        if (/^[01]*$/.test(value)){
+            setInput(event.target.value);
+            setShowError(false);
+        }
+        else {
+            //const inputField = document.getElementById('binary-input');
+          //  const errorDiv = document.createElement('div');
+           /// errorDiv.classList.add('error-popup');
+            ///errorDiv.textContent = 'Binary digits only';
+           // document.querySelector('.binary-input').appendChild(errorDiv);
+           setShowError(true);
+        }
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            handleButtonClick();
+        }
     }
 
 
     return (
         <div className="modal">
             <OutputField data={data}></OutputField>
-            <InputField value={input} onClick={handleButtonClick} onChange={handleInputChange} ></InputField>
+            <InputField value={input} onKeyDown={handleKeyDown} onClick={handleButtonClick} onChange={handleInputChange} ></InputField>
+            {showError && (
+                    <div className="error-popup">Binary digits only</div>
+                )}
         </div>
     )
 }
